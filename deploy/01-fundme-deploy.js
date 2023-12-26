e7ada5ac18db5b3555f139a4e2a37c56e616023b
Sunil Reddy <sunday7637@gmail.com>
2023-12-26 11:10:50 +0530

    update

---
+++ deploy/01-fundme-deploy.js
@@ -0,0 +1,8 @@
+module.exports = async ({getNamedAccounts,
+  deployments,
+  getChainId,
+  getUnnamedAccounts,})=>{
+    console.log("hello sunil")
+    
+
+  }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,13 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('hardhat-deploy');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.19",
+  namedAccounts: {
+    deployer: {
+        default: 0, 
+        1: 0, 
+    },
+},
 };
