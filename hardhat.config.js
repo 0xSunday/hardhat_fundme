b316d763b2baea7d0f03e1f20ed9b2915ea01390
Sunil Reddy <sunday7637@gmail.com>
2023-12-26 13:07:00 +0530

    added more

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,13 +1,50 @@
 require("@nomicfoundation/hardhat-toolbox");
-require('hardhat-deploy');
+require("hardhat-deploy");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const API_KEY = process.env.ETHER_SCAN_API_KEY;
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accouts: [PRIVATE_KEY],
+      chainId: 11155111,
+      blockConfirmations: 6,
+    },
+  },
+  // solidity: "0.8.19",
+  // solidity:  "0.8.7",,
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+      {
+        version: "0.6.6",
+      },
+    ],
+  },
+  gasReporter: {
+    enabled: true,
+    currency: "USD",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    // coinmarketcap: COINMARKETCAP_API_KEY,
+  },
   namedAccounts: {
     deployer: {
-        default: 0, 
-        1: 0, 
+      default: 0,
+      1: 0,
     },
-},
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: API_KEY,
+  },
 };
